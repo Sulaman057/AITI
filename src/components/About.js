0c0d0388ce71fa285990e9ecd0815b91274f1197
Sulaman057 <sulamanmuhammad057@gmail.com>
2023-08-19 02:20:22 +0500

    lint problem

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ const About = (props) => {
       <div className="">
         <h2>About Us</h2>
         <h3 className="mb-2">Elevating Your Business with Next-Generation IT Services</h3>
-        <p>At AITI, we are not just another IT company. We are a trailblazer in the realm of advanced technology solutions. With a legacy of delivering cutting-edge IT services, we stand as a beacon of innovation and excellence in the digital landscape. Our team of experts is dedicated to pushing the boundaries of what's possible, ensuring that businesses stay ahead in an ever-evolving technological world.</p>
+        <p>At AITI, we are not just another IT company. We are a trailblazer in the realm of advanced technology solutions. With a legacy of delivering cutting-edge IT services, we stand as a beacon of innovation and excellence in the digital landscape. Our team of experts is dedicated to pushing the boundaries of whats possible, ensuring that businesses stay ahead in an ever-evolving technological world.</p>
       </div>
       <Image 
         className="lg:w-[700px] lg:h-auto xs:hidden"
